3d40fd7b8d335db22cfea2b3b3bc173628d304c5
artempal <mail@artempal.ru>
2016-11-09 14:56:57 +0300

    шаблоны

--- app1/app1/app1.cpp
+++ app1/app1/app1.cpp
@@ -5,9 +5,26 @@
 #include <iostream>
 
 using namespace std;
+
+template <typename T>
+void my_swap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+template <typename T>
+class Templ {
+private:
+	T pnop;
+};
 int main()
 {
-	cout << "Hello world!" << endl;
+	int a = 2;
+	int b = 3;
+	my_swap(a, b);
+	my_swap<int>(a, b);
+	cout << a << b << endl;
+	Templ<int> obj;
     return 0;
 }
-
