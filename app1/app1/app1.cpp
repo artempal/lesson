42ecd0629b0bf8a891c117efa355524af55fee53
artempal <mail@artempal.ru>
2016-11-09 08:50:10 +0300

    новый проект

---
+++ app1/app1/app1.cpp
@@ -0,0 +1,13 @@
+// app1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello world!" << endl;
+    return 0;
+}
+
