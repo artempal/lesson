64d16af9ec28ae15d5a1e8e2b101b7ec43444ee8
Artem <mail@artempal.ru>
2016-12-18 14:25:25 +0300

    готовое задание с фигурой

--- figures/figures/figures.cpp
+++ figures/figures/figures.cpp
@@ -11,9 +11,8 @@ class Figura {
 protected:
 	int _col_storon;
 	int* _dlina;
-	
-public:
 	static int _col_figur;
+public:
 	static int col_figur()
 	{
 		return _col_figur;
@@ -25,10 +24,10 @@ public:
 	Figura()
 	{
 		_col_figur++;
-		for (int i = 0; i<_col_storon; i++)
-		{
-			_dlina[i] = _dlina[i];
-		}
+	}
+	~Figura()
+	{
+		delete _dlina;
 	}
 	virtual int perimetr()
 	{
@@ -42,81 +41,115 @@ public:
 	virtual double ploshad()
 	{
 		int pl = _dlina[0];
-		for (int i = 1; i<_col_storon; i++)
+		for (int i = 1; i<_col_storon-2; i++)
 		{
 			pl *= _dlina[i];
 		}
 		return pl;
 	}
 };
+int Figura::_col_figur = 0;
 
-class Kvadrat : public Figura {
+class Rectangle : public Figura {
 public:
-	Kvadrat()
+	Rectangle(int a, int b)
 	{
 		_col_storon = 4;
 		_dlina = new int[4];
-		for (int i = 0; i<4; i++)
-		{
-			_dlina[i] = 2;
-		}
+		_dlina[0] = a;
+		_dlina[1] = b;
+		_dlina[2] = a;
+		_dlina[3] = b;
+	}
+	double diag()
+	{
+		return sqrt(pow(_dlina[0], 2) + pow(_dlina[1], 2));
 	}
 };
 class Treug : public Figura {
-protected:
-	int _h;
 public:
-	Treug();
-	Treug(int h)
+	Treug() 
+	{
+
+	}
+	Treug(int a, int b, int c)
 	{
 		_col_storon = 3;
 		_dlina = new int[3];
-		_h = h;
-		for (int i = 0; i<3; i++)
-		{
-			_dlina[i] = 2;
-		}
+		_dlina[0] = a;
+		_dlina[1] = b;
+		_dlina[2] = c;
 	}
 	double ploshad()
 	{
-		double pl;
-		pl = (1 / 2)*_dlina[1] * _h;
+		double p = (double)perimetr() / 2;
+		double pl = sqrt(p*(p- _dlina[0])*(p- _dlina[1])*(p- _dlina[2]));
 		return pl;
+	}
+	int perimetr()
+	{
+		int p;
+		p = _dlina[0] + _dlina[1] + _dlina[2];
+		return p;
 	}
 };
 class ravnobedrTreug : public Treug {
 public:
-	ravnobedrTreug(int h)
+	ravnobedrTreug(int a, int b)
 	{
-		_col_storon = 2;
-		_dlina = new int[2];
-		_h = h;
-		for (int i = 0; i<2; i++)
-		{
-			_dlina[i] = 2;
-		}
+		_col_storon = 3;
+		_dlina = new int[3];
+		_dlina[0] = a;
+		_dlina[1] = b;
+		_dlina[2] = a;
+	}
+	double h()
+	{
+		return sqrt(pow(_dlina[0], 2)-pow(_dlina[1], 2) / 4);
 	}
 };
 int main()
 {
-	Figura *f = new Figura;
-	Kvadrat *k = new Kvadrat();
-	int perim = k->perimetr();
-	double pl = k->ploshad();
-	cout << "Perimetr = " << perim << endl;
-	cout << "Ploshad = " << pl << endl;
+	setlocale(LC_ALL, "rus");
+
+	cout << "----------------" << endl;
+	cout << "Создаем прямоугольник со сторонами 5 и 4" << endl;
+	Rectangle *k = new Rectangle(5,4);
+
+	cout << "Периметр прямоугольника = " << k->perimetr() << endl;
+	cout << "Площадь прямоугольника = " << k->ploshad() << endl;
+	cout << "Диагональ прямоугольника = " << k->diag() << endl;
+
+	cout << "----------------" << endl;
+	cout << "Создаем треугольник со строрнами 3, 4, 5" << endl;
+	Treug *t = new Treug(3,4,5);
+
+	cout << "Периметр треугольника = " << t->perimetr() << endl;
+	cout << "Площадь треугольника = " << t->ploshad() << endl;
+
+	cout << "----------------" << endl;
+	cout << "Создаем равнобедренный треугольник со строрнами 4 и 5" << endl;
+	ravnobedrTreug *rt = new ravnobedrTreug(4,5);
+
+	cout << "Периметр равнобедренного треугольника = " << rt->perimetr() << endl;
+	cout << "Площадь равнобедренного треугольника = " << rt->ploshad() << endl;
+	cout << "Высота равнобедренного треугольника = " << rt->h() << endl;
+
+	cout << "----------------" << endl;
+	cout << "Количество созданных фигур = " << Figura::col_figur() << endl;
+	cout << "----------------" << endl;
+	int colvo;
+	cout << "Введите новое количество фигур: " << endl;
+	cin >> colvo;
+	cout << "----------------" << endl;
+	Figura::replace_col_figur(colvo);
+	cout << "Количество созданных фигур после смены значения = " << Figura::col_figur() << endl;
+	cout << "----------------" << endl;
 
 
-	Figura *ptr;
-	int h = 5;
-	Treug *t = new Treug(h);
+	delete k;
+	delete rt;
 
-	ptr = t;
-	cout << "Ploshad virtual = " << ptr->ploshad() << endl;
-	cout << "Col-vo figur: =" << Figura::col_figur() << endl;
-	int col = 125;
-	Figura::replace_col_figur(col);
-	cout << "Col-vo figur: =" << Figura::col_figur() << endl;
     return 0;
 }
 
