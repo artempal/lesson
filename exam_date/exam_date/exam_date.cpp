f2d2d604d8ccdca56ade558992a43c4ade429281
Artem <mail@artempal.ru>
2017-01-19 14:09:44 +0300

    изменение в программе даты

--- exam_date/exam_date/exam_date.cpp
+++ exam_date/exam_date/exam_date.cpp
@@ -3,7 +3,6 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <ctime>
 
 
 using namespace std;
@@ -13,15 +12,63 @@ class Date
 private:
 	int D1[3]; //формат даты ГГГГ-ММ-ДД
 	int D2[3];
-	int days; //сколько дней между датами
 public:
-	void dist()
+	Date(int y1, int m1, int d1, int y2, int m2, int d2)
 	{
-
+		D1[0] = y1;
+		D1[1] = m1;
+		D1[2] = d1;
+		D2[0] = y2;
+		D2[1] = m2;
+		D2[2] = d2;
 	}
-	void arr_to_day(int *date)
+	int dist() //разница между двумя датами
 	{
-		unsigned int days = date[0] * (bissextile(date[0])?366:365) + date[1] * days_in_month(date[1], date[0]) + date[2];
+		return arr_to_day(D2) - arr_to_day(D1);
+	}
+	void minus(int day) //вычитание дней из даты
+	{
+		int days = arr_to_day(D1) - day;
+		int *D;
+		D = days_to_arr(days);
+		cout << D[0] << "." << D[1] << "." << D[2] << endl;
+	}
+	int* days_to_arr(int days) //перевод дней в массив
+	{
+		int D[3];
+		int sum_day_y = 0;
+		int sum_day_m = 0;
+		int y = 1;
+		int m = 1;
+		int d = 1;
+		while ((sum_day_y + (bissextile(y+1) ? 366 : 365)) <= days)
+		{
+			sum_day_y += bissextile(y) ? 366 : 365;
+			y++;
+		}
+		days -= sum_day_y;
+		while (sum_day_m + days_in_month(m + 1, y) <= days) 
+		{
+			sum_day_m += days_in_month(m, y);
+			m++;
+		}
+		days -= sum_day_m;
+		d += days;
+		D[0] = y;
+		D[1] = m;
+		D[2] = d - 1; //вычтем текущий день
+		return D;
+	}
+	int arr_to_day(int *date) //перевод массива в дни
+	{
+		int sum_day_y = 0;
+		int sum_day_m = 0;
+		for (int i = 1; i < date[0]; i++)
+			sum_day_y += bissextile(i) ? 366 : 365;
+		for (int i = 1; i < date[1]; i++)
+			sum_day_m += days_in_month(i, date[0]);
+		int days = sum_day_y + sum_day_m + date[2];
+		 return days;
 	}
 	int bissextile(int y) //проверка високосного года
 	{
@@ -31,11 +78,14 @@ public:
 	{
 		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 		if (bissextile(y))days[1] = 29;
-		return days[m-1];
+		return days[m - 1];
 	}
 };
 int main()
 {
+	Date date(2017, 1, 19, 2018, 10, 19);
+	cout << date.dist() << endl;
+	date.minus(3);
     return 0;
 }
 
